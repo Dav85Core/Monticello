eac955426903e67eca5ef6619580b68e0316ee92
DAVIDE CORDA <AMAZINGDAVE85@gmail.com>
2023-01-04 11:27:40 +0100

    modificato

--- main.js
+++ main.js
@@ -3,20 +3,21 @@ $(document).ready(function () {
     slidesToShow: 3,
     slidesToScroll: 1,
   });
+  function initMap(){
+    console.log('map inited')
+    new google.maps.Map(document.getElementById('map'), {
+        center: {lat:40.790649,lng: -73.969082},
+        zoom: 4, 
+    })
+    
+    
+  }
+  initMap();
 });
 
-function initMap(){
-  console.log('map inited')
-  var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat:40.790649,lng: -73.969082},
-      zoom: 4, 
-  })
-  
-  
-}
 
 
 
 
-initMap();
+
 
